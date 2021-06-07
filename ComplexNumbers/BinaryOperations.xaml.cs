e2b2ccd7bd3222328ae85d6106c254ce2d3a680f
Vizir Constantin <vizirconstantin.89@gmail.com>
2021-06-07 22:24:26 +0300

    Complex numbers repository first commit

---
+++ ComplexNumbers/BinaryOperations.xaml.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Numerics;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ComplexNumbers
+{
+    /// <summary>
+    /// Interaction logic for BinaryOperations.xaml
+    /// </summary>
+    public partial class BinaryOperations : Window
+    {
+        public BinaryOperations()
+        {
+            InitializeComponent();
+        }
+
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9.-]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private void ClearTextbox()
+        {
+            this.real_1.Clear();
+            this.real_2.Clear();
+            this.imaginary_1.Clear();
+            this.imaginary_2.Clear();
+        }
+
+        private void ShowError()
+        {
+            Error er = new Error();
+            er.Show();
+        }
+        private void real_1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ClearTextbox();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                double r1 = Double.Parse(real_1.Text);
+                double r2 = Double.Parse(real_2.Text);
+                double i1 = Double.Parse(imaginary_1.Text);
+                double i2 = Double.Parse(imaginary_2.Text);
+                Complex z1 = new Complex(r1, i1);
+                Complex z2 = new Complex(r2, i2);
+                answer.Text = Complex.Add(z1, z2).ToString();
+            }
+            catch (Exception)
+            {
+                ShowError();
+            }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                double r1 = Double.Parse(real_1.Text);
+                double r2 = Double.Parse(real_2.Text);
+                double i1 = Double.Parse(imaginary_1.Text);
+                double i2 = Double.Parse(imaginary_2.Text);
+                Complex z1 = new Complex(r1, i1);
+                Complex z2 = new Complex(r2, i2);
+                answer.Text = Complex.Subtract(z1, z2).ToString();
+            }
+            catch (Exception)
+            {
+                ShowError();
+            }
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                double r1 = Double.Parse(real_1.Text);
+                double r2 = Double.Parse(real_2.Text);
+                double i1 = Double.Parse(imaginary_1.Text);
+                double i2 = Double.Parse(imaginary_2.Text);
+                Complex z1 = new Complex(r1, i1);
+                Complex z2 = new Complex(r2, i2);
+                answer.Text = Complex.Multiply(z1, z2).ToString();
+            }
+            catch (Exception)
+            {
+                ShowError();
+            }
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                double r1 = Double.Parse(real_1.Text);
+                double r2 = Double.Parse(real_2.Text);
+                double i1 = Double.Parse(imaginary_1.Text);
+                double i2 = Double.Parse(imaginary_2.Text);
+                Complex z1 = new Complex(r1, i1);
+                Complex z2 = new Complex(r2, i2);
+                answer.Text = Complex.Divide(z1, z2).ToString();
+            }
+            catch (Exception)
+            {
+                ShowError();
+            }
+        }
+    }
+}
---
+++ ComplexNumbers/UnaryOperations.xaml.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Numerics;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Text.RegularExpressions;
+
+namespace ComplexNumbers
+{
+    /// <summary>
+    /// Interaction logic for UnaryOperations.xaml
+    /// </summary>
+    public partial class UnaryOperations : Window
+    {
+        public UnaryOperations()
+        {
+            InitializeComponent();
+        }      
+
+        private void ClearTextbox()
+        {
+            this.realPart.Clear();
+            this.imaginaryPart.Clear();
+        }
+
+        private void ShowError()
+        {
+            Error er = new Error();
+            er.Show();      
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ClearTextbox();
+        }
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9.-]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+        
+        //Magnitude Button
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            bool exceptionThrown = false;
+            try
+            {
+                double real = Double.Parse(realPart.Text);
+                double imaginary = Double.Parse(imaginaryPart.Text);
+                Complex z = new Complex(real, imaginary);
+                answerBox.Text = z.Magnitude.ToString();
+            }
+            catch (Exception)
+            {
+                ShowError();
+                exceptionThrown = true;
+            }
+            finally
+            {
+                if(exceptionThrown)
+                   ClearTextbox();
+            }           
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            bool exceptionThrown = false;
+            try
+            {
+                double real = Double.Parse(realPart.Text);
+                double imaginary = Double.Parse(imaginaryPart.Text);
+                Complex z = new Complex(real, imaginary);
+                answerBox.Text = Complex.Conjugate(z).ToString();
+            }
+            catch (Exception)
+            {
+                ShowError();
+                exceptionThrown = true;
+            }
+            finally
+            {
+                if (exceptionThrown)
+                    ClearTextbox();
+            }
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            bool exceptionThrown = false;
+            try
+            {
+                double real = Double.Parse(realPart.Text);
+                double imaginary = Double.Parse(imaginaryPart.Text);
+                Complex z = new Complex(real, imaginary);
+                answerBox.Text = (z.Phase * 180/ Math.PI).ToString();
+            }
+            catch (Exception)
+            {
+                ShowError();
+                exceptionThrown = true;
+            }
+            finally
+            {
+                if (exceptionThrown)
+                    ClearTextbox();
+            }
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            bool exceptionThrown = false;
+            try
+            {
+                double real = Double.Parse(realPart.Text);
+                double imaginary = Double.Parse(imaginaryPart.Text);
+                Complex z = new Complex(real, imaginary);
+                answerBox.Text = Complex.Log(z).ToString();
+            }
+            catch (Exception)
+            {
+                ShowError();
+                exceptionThrown = true;
+            }
+            finally
+            {
+                if (exceptionThrown)
+                    ClearTextbox();
+            }
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+            bool exceptionThrown = false;
+            try
+            {
+                double real = Double.Parse(realPart.Text);
+                double imaginary = Double.Parse(imaginaryPart.Text);
+                double pow = Double.Parse(power.Text);
+                Complex z = new Complex(real, imaginary);
+                answerBox.Text = Complex.Pow(z, pow).ToString();
+            }
+            catch (Exception)
+            {
+                ShowError();
+                exceptionThrown = true;
+            }
+            finally
+            {
+                if (exceptionThrown)
+                    ClearTextbox();
+            }
+        }
+    }
+}
